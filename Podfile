13bedbf3f8c21e82074d1a3153c9d786df19a7a1
skaldat1 <skaldat1@binghamton.edu>
2019-01-30 09:50:10 -0500

    final commit

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'basic-ios-app' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+  pod 'lottie-ios'
+  pod 'MaterialComponents/Buttons'
+  
+  # Pods for basic-ios-app
+
+  target 'basic-ios-appTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'basic-ios-appUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- basic-ios-app/ViewController.m
+++ basic-ios-app/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import <Lottie/Lottie.h>
+#import <UIKit/UIKit.h>
+#import <MaterialButtons.h>
 
 @interface ViewController ()
 
@@ -17,7 +20,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    MDCButton *button = [[MDCButton alloc] init];
+    [button setTitle:@"Start Course Journey" forState:UIControlStateNormal];
+    [button setFrame:CGRectMake(100, 500, 210, 48)];
+    [button addTarget:self action:@selector(didTapStart:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
 }
 
-
+- (void)didTapStart:(id)sender {
+    LOTAnimationView *animation = [LOTAnimationView animationNamed:@"funky-chicken.json"];
+    animation.loopAnimation = true;
+    [animation setFrame:CGRectMake(15, 390, 400, 400)];
+    [self.view addSubview:animation];
+    [animation playWithCompletion:^(BOOL animationFinished) {
+        // Do Something
+    }];
+}
 @end
